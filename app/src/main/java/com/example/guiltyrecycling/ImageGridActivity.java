3deda6d9c2f5cc537ab995cecf6e91b017aac673
baumflektatwit <baumflekt@wit.edu>
2022-07-19 12:20:44 -0400

    fixed main activity & xml file

--- app/src/main/java/com/example/guiltyrecycling/ImageGridActivity.java
+++ app/src/main/java/com/example/guiltyrecycling/ImageGridActivity.java
@@ -17,7 +17,7 @@ public class ImageGridActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.image_grid_activity);
 
         gridView = (GridView) findViewById(R.id.GridView01);
 
