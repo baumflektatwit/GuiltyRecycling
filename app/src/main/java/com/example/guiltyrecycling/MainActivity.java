562c71d10199381e14932fbcf9949f2c962204f1
Tsuf I Baumflek <baumflekt@wit.edu>
2022-07-19 11:38:47 -0400

    just a quick test

--- app/src/main/java/com/example/guiltyrecycling/MainActivity.java
+++ app/src/main/java/com/example/guiltyrecycling/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.guiltyrecycling;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 //testing
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Test 5
+        // hey this is tsuf testing
     }
 }
